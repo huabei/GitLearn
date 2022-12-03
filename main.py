3f57ad4ff459728f99ab8f76147abbfadcfd3bdd
王华北 <951946366@qq.com>
2022-12-03 10:20:05 +0800

    "today update"

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import subprocess
 if __name__ == "__main__":  
     os.chdir(r"E:\Python_Project\GitLearn")
     subprocess.call([r"D:\Program Files\SmartGit\git\bin\git.exe", 'commit', '-a', '-m', '"today update"'])    
-    subprocess.call([r'D:\Program Files\SmartGit\git\bin\git.exe', 'git', 'push'])
+    subprocess.call([r'D:\Program Files\SmartGit\git\bin\git.exe', 'push'])
     
