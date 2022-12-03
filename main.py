0ca9bb8306408907e24270bb59ebb6ef71a15c54
王华北 <951946366@qq.com>
2022-12-03 10:19:45 +0800

    "today update"

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+
+if __name__ == "__main__":  
+    os.chdir(r"E:\Python_Project\GitLearn")
+    subprocess.call([r"D:\Program Files\SmartGit\git\bin\git.exe", 'commit', '-a', '-m', '"today update"'])    
+    subprocess.call([r'D:\Program Files\SmartGit\git\bin\git.exe', 'git', 'push'])
+    
